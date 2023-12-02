be5e8402c25dde09fa4d5ba08785c2ad7c762d3a
danielzaenker <44169349+danielzaenker@users.noreply.github.com>
2023-12-02 08:53:24 +0100

    complete: day02

--- src/main/kotlin/net/codetreats/aoc/day02/Day02.kt
+++ src/main/kotlin/net/codetreats/aoc/day02/Day02.kt
@@ -2,19 +2,61 @@ package net.codetreats.aoc.day02
 
 import net.codetreats.aoc.Day
 import net.codetreats.aoc.util.Logger
+import kotlin.math.max
 
 class Day02 : Day<List<String>>(2) {
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
-    override val useDummy = true
+    override val useDummy = false
 
     override fun convert(input: List<String>): List<String> = input
 
     override fun run1(data: List<String>): String {
-        return ""
+        val maxColor = mapOf(
+            "red" to 12,
+            "green" to 13,
+            "blue" to 14,
+        )
+        var possible = 0
+        data.forEach { line ->
+            val split = line.split(":")
+            val id = split.first().split(" ").last().toInt()
+            val turns = split.last().split(";")
+            val isImpossible = turns.any { turn ->
+                val cubes = turn.split(",")
+                cubes.any { cube ->
+                    val color = cube.split(" ").filter { it.isNotEmpty() }
+                    color.first().toInt() > maxColor[color.last()]!!
+                }
+            }
+            if (!isImpossible) {
+                possible += id
+            }
+        }
+        return possible.toString()
     }
 
     override fun run2(data: List<String>): String {
-        return ""
+        var sum = 0
+        data.forEach { line ->
+            val split = line.split(":")
+            val turns = split.last().split(";")
+            val minColor = mutableMapOf(
+                "red" to 0,
+                "green" to 0,
+                "blue" to 0,
+            )
+            turns.forEach { turn ->
+                val cubes = turn.split(",")
+                cubes.forEach { cube ->
+                    val color = cube.split(" ").filter { it.isNotEmpty() }
+                    val curMin = minColor[color.last()]!!
+                    minColor[color.last()] = max(color.first().toInt(), curMin)
+                }
+            }
+            val power =  minColor["red"]!! * minColor["green"]!! * minColor["blue"]!!
+            sum += power
+        }
+        return sum.toString()
     }
 }
