7696de6ff1eef40e88a7678e1e998441570586b4
danielzaenker <44169349+danielzaenker@users.noreply.github.com>
2023-12-04 10:39:02 +0100

    completed: day04

--- src/main/kotlin/net/codetreats/aoc/day04/Day04.kt
+++ src/main/kotlin/net/codetreats/aoc/day04/Day04.kt
@@ -3,18 +3,49 @@ package net.codetreats.aoc.day04
 import net.codetreats.aoc.Day
 import net.codetreats.aoc.util.Logger
 
-class Day04 : Day<List<String>>(4) {
+// only for numbers >= 0
+private fun Int.power(x: Int): Int {
+    var result = 1
+    for (i in 0..<x) {
+        result *= this
+    }
+    return result
+}
+
+class Day04 : Day<List<Int>>(4) {
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
-    override val useDummy = true
+    override val useDummy = false
 
-    override fun convert(input: List<String>): List<String> = input
-
-    override fun run1(data: List<String>): String {
-        return ""
+    override fun convert(input: List<String>): List<Int> {
+        return input.map { line ->
+            val (winning, drawn) = line.split(":").last().split("|").map {
+                it.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            }
+            drawn.count { it in winning }
+        }
     }
 
-    override fun run2(data: List<String>): String {
-        return ""
+    override fun run1(data: List<Int>): String {
+        return data.sumOf { won ->
+            if (won == 0) {
+                0
+            } else {
+                2.power(won-1)
+            }
+        }.toString()
+    }
+
+    override fun run2(data: List<Int>): String {
+        val counts = MutableList(data.size) { 1 }
+        data.forEachIndexed { idx, won ->
+            val cardCount = counts[idx]
+            if (won > 0) {
+                for (countIdx in idx+1..idx+won) {
+                   counts[countIdx] += cardCount
+                }
+            }
+        }
+        return counts.sum().toString()
     }
 }
--- src/test/kotlin/net/codetreats/aoc/day04/Day04Test.kt
+++ src/test/kotlin/net/codetreats/aoc/day04/Day04Test.kt
@@ -17,21 +17,21 @@ class Day04Test {
 
     @Test
     fun testPart01() {
-        assertEquals("", classUnderTest.run1(false))
+        assertEquals("17782", classUnderTest.run1(false))
     }
 
     @Test
     fun testPart01_dummyData() {
-        assertEquals("", classUnderTest.run1(true))
+        assertEquals("13", classUnderTest.run1(true))
     }
 
     @Test
     fun testPart02() {
-        assertEquals("", classUnderTest.run2(false))
+        assertEquals("8477787", classUnderTest.run2(false))
     }
 
     @Test
     fun testPart02_dummyData() {
-        assertEquals("", classUnderTest.run2(true))
+        assertEquals("30", classUnderTest.run2(true))
     }
 }
