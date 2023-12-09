29c41ccd285804dc6e3445cf84d48270036b1fb9
danielzaenker <44169349+danielzaenker@users.noreply.github.com>
2023-12-09 10:21:22 +0100

    refactor: day08

--- src/main/kotlin/net/codetreats/aoc/day08/Day08.kt
+++ src/main/kotlin/net/codetreats/aoc/day08/Day08.kt
@@ -3,22 +3,21 @@ package net.codetreats.aoc.day08
 import net.codetreats.aoc.Day
 import net.codetreats.aoc.util.Logger
 
-private data class Node(var left: Node?, var right: Node?, val content: String) {
+data class Node(var left: Node?, var right: Node?, val content: String) {
     override fun toString(): String {
         return "Node(left=${left?.content}, right=${right?.content}, content='$content')"
     }
 }
 
-class Day08 : Day<List<String>>(8) {
+class Day08 : Day<MutableMap<String, Node>>(8) {
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
     override val useDummy = false
 
-    private val nodeMap = mutableMapOf<String, Node>()
     private var steps: String = ""
 
-    override fun convert(input: List<String>): List<String> {
-        nodeMap.clear()
+    override fun convert(input: List<String>): MutableMap<String, Node> {
+        val nodeMap = mutableMapOf<String, Node>()
         steps = input.first().trim()
         input.drop(2).filter { it.isNotBlank() }.forEach { line ->
             val (key, children) = line.split("=").map { it.trim() }
@@ -33,7 +32,7 @@ class Day08 : Day<List<String>>(8) {
             nodeMap[key] = node
         }
 
-        return input
+        return nodeMap
     }
 
     private fun stepsNeeded(start: Node, end: String): Long {
@@ -54,23 +53,23 @@ class Day08 : Day<List<String>>(8) {
         }
     }
 
-    override fun run1(data: List<String>): String {
-        return stepsNeeded(nodeMap["AAA"]!!, "ZZZ").toString()
+    override fun run1(data: MutableMap<String, Node>): String {
+        return stepsNeeded(data["AAA"]!!, "ZZZ").toString()
     }
 
     // https://rosettacode.org/wiki/Least_common_multiple#Kotlin
     private fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
     private fun lcm(a: Long, b: Long): Long = a / gcd(a, b) * b
 
-    private fun List<Long>.lcm() : Long {
+    private fun List<Long>.toLcm() : Long {
         return this.reduce { cur, value -> lcm(cur, value) }
     }
 
-    override fun run2(data: List<String>): String {
-        val starts = nodeMap.filter { (k, v) -> v.content.endsWith("A") }.values
+    override fun run2(data: MutableMap<String, Node>): String {
+        val starts = data.filter { (k, v) -> v.content.endsWith("A") }.values
         val destinations = starts.map { start ->
             stepsNeeded(start, "Z")
         }
-        return destinations.lcm().toString()
+        return destinations.toLcm().toString()
     }
 }
